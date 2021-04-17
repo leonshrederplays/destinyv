6491963f3d46a2a03c719cd38363fc1b0cc5667a
Kristian <kristianpavic70@gmail.com>
2021-04-17 19:36:00 +0200

    HTML Fehler behoben

--- resources/Login/client/client.js
+++ resources/Login/client/client.js
@@ -13,43 +13,21 @@ function startLogin(){
     let webview;
 
     if (!webview){
-        webview = new alt.WebView('../html/index.html');
+        webview = new alt.WebView('http://resource/client/html/index.html');
         webview.on('close:WebView', closeWebview);
+        webview.focus();
+      alt.showCursor(true);
       }
+}
     
-      webview.focus();
-      alt.showCursor(true);}
+    
+      
 
 
 
       function closeWebview(){
+        webview.unfocus();
         alt.showCursor(true);
         webview.destroy();
         webview = undefined;
       }
-
-
-
-
-
-
-// This event fires when the chat is opened.
-alt.on('chatOpened', () => {
-    alt.log('Chat was opened!');
-});
-
-// This event fires when the chat is closed.
-alt.on('chatClosed', () => {
-    alt.log('Chat was closed!');
-});
-
-// This event is fired when the player presses enter or the chat message is longer than 1.
-alt.on('messageSent', (msg) => {
-    alt.log(msg);
-});
-
-chat.pushMessage('tacoGuy', 'hello');
-chat.pushLine('hello there!');
-
-var chatHiddenStatus = chat.isChatHidden();
-var chatOpenStatus = chat.isChatOpen();
